64dbc9983bf8a994e6f2b3990bbba6601c50d1ad
sowmyavenu <92860289+sowmyavenu@users.noreply.github.com>
2022-05-20 17:56:52 +0530

    Update streamlit_app.py
    Check- UT

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ streamlit.dataframe(my_fruit_list)
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display table
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
-#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-#fruits_to_show=my_fruit_list.loc(fruits_selected)
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show=my_fruit_list.loc(fruits_selected)
 
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)
