193b9708179c62237485e4ab54ef39db967359e8
sowmyavenu <92860289+sowmyavenu@users.noreply.github.com>
2022-05-20 17:52:44 +0530

    Update streamlit_app.py
    Removed duplicate lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,10 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
 
 #Pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display table
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc(fruits_selected)
